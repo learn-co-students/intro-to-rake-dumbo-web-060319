43ca8fd73a8642ab57afd6c7c1f7e6b9f6266561
Kevin <kevinlai76@gmail.com>
2019-06-13 23:19:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,15 +17,15 @@ class Student
   
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade TEXT
-        )
-    SQL
-    DB[:conn].execute(sql) 
-  end
-
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade TEXT
+          )
+      SQL
+      DB[:conn].execute(sql) 
+    end
+  
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql) 
